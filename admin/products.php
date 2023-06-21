517c57fd049d1cde6574b0bc31c63e60f80a4ab5
AmineBoussetta <amineboussetta006@gmail.com>
2023-06-21 20:06:58 +0100

    edit admin

---
+++ admin/products.php
@@ -0,0 +1,145 @@
+<?php include('header.php'); ?>
+
+<?php
+    if(!isset($_SESSION['admin_logged_in'])){
+        header('location: login.php');
+        exit;
+    }
+
+?>
+
+<?php
+    
+    //1 determine page no
+    if(isset($_GET['page_no']) && $_GET['page_no'] != ""){
+        //if user has already entered the page then page number is the one that they selected
+        $page_no = $_GET['page_no'];
+    }else{
+        //if user just entered the page then default page is 1
+        $page_no = 1;
+    }
+
+    //2 return number of products
+    $stmt1 = $conn->prepare("SELECT COUNT(*) AS total_records FROM products");
+    $stmt1->execute();
+    $stmt1->bind_result($total_records);
+    $stmt1->store_result();
+    $stmt1->fetch();
+
+    //3 products per page
+    $total_records_per_page = 10;
+
+    $offset = ($page_no - 1) * $total_records_per_page;
+
+    $previous_page = $page_no - 1;
+    $next_page = $page_no + 1;
+
+    $adjacents = "2";
+
+    $total_no_of_pages = ceil($total_records/$total_records_per_page);
+
+    //4 get all products
+
+    $stmt2 = $conn->prepare("SELECT * FROM products LIMIT $offset, $total_records_per_page");
+    $stmt2->execute();
+    $products = $stmt2->get_result();
+
+
+  
+
+?>
+
+
+
+
+<div class="container-fluid">
+    <div class="row">
+
+
+        <?php include('sidemenu.php'); ?>
+        
+
+
+    
+        <main class="col-md-9 ms_sm_auto col_lg-10 px_md-4">
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+                <h1 class="h2">Dashboard</h1>
+                <div class="btn-toolbar mb-2 mb-md-0">
+                    <div class="btn-group me-2">
+
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- Database config needed -->
+            <h2>Products</h2>
+            <div class="table-responsive">
+                <table class="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                            <th scope="col">Product Id</th>
+                            <th scope="col">Product Image</th>
+                            <th scope="col">Product Name</th>
+                            <th scope="col">Product Price</th>
+                            <th scope="col">Product Category</th>
+                            <th scope="col">Product Size</th>
+                            <th scope="col">Product UpImage</th>
+                            <th scope="col">Edit</th>
+                            <th scope="col">Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($products as $order){ ?>
+                        <tr>
+                            <td><?php echo $product['product_id']; ?></td>
+                            <td><img src="<?php echo " ../assets/imgs/". $product['product_image']; ?>" style="width: 70px; height:70px"/></td>
+                            <td><?php echo $product['product_name']; ?></td>
+                            <td><?php echo "Dt". $product['product_price']; ?></td>
+                            <td><?php echo $product['product_type']; ?></td>
+                            <td><?php echo $product['product_size']; ?></td>
+                            td><?php echo $product['product_up_img']; ?></td>
+
+                            <td><a class="btn btn-primary" href="edit_product.php?product_id=<?php echo $product['product_id']; ?>">Edit</a></td>
+                            <td><a class="btn btn-danger">Delete</a></td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+
+                <!-- pagination -->
+                <nav aria-label="Page navigation example" class="mx-auto">
+                    <ul class="pagination mt-5 mx-auto">
+
+                        <li class="page-item <?php if($page_no <= 1){echo 'disabled';}?> ">
+                            <a href="<?php if($page_no <= 1){echo '#';}else{echo "?page_no=".($page_no-1);} ?>" class="page-link">Previous</a>
+                        </li>
+
+                        <li class="page-item"><a class="page-link" href="?page_no=1">1</a></li>
+                        <li class="page-item"><a class="page-link" href="?page_no=2">2</a></li>
+
+                        <?php if($page_no >= 3){ ?>
+                        <li class="page-item"><a class="page-link" href="">...</a></li>
+                        <li class="page-item"><a class="page-link" href="<?php echo "?page_no=".$page_no; ?>"><?php echo $page_no; ?></a></li>
+
+                        <?php } ?>
+
+                        <li class="page-item <?php if($page_no >= $total_no_of_pages){echo'disabled';}?>">
+                            <a class="page-link" href=" <?php if($page_no >= $total_no_of_pages){echo '#';}else{echo "?page_no=".($page_no+1);} ?>">Next</a></li>
+                    </ul>
+                </nav>
+
+
+
+
+
+
+
+
+
+
+            </div>
+
+        </main>
+    </div>
+</div>
